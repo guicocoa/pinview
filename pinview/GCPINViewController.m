7b85485326807f00ed9784b74ec294a3261fd0d3
Caleb Davenport <caleb@guicocoa.com>
2012-01-01 10:53:16 -0500

    minor code changes

--- pinview/GCPINViewController.m
+++ pinview/GCPINViewController.m
@@ -9,14 +9,31 @@
 #import "GCPINViewController.h"
 #import <AudioToolbox/AudioToolbox.h>
 
+#define kGCPINViewControllerDelay 0.3
+
 @interface GCPINViewController ()
+
+// array of passcode entry labels
 @property (copy, nonatomic) NSArray *labels;
-@property (copy, nonatomic) NSString *text;
+
+// readwrite override for mode
 @property (nonatomic, readwrite, assign) GCPINViewControllerMode mode;
-- (void)updatePINDisplay;
-- (void)reset;
+
+// extra storage used when creating a passcode
+@property (copy, nonatomic) NSString *text;
+
+// make the passcode entry labels match the input text
+- (void)updatePasscodeDisplay;
+
+// reset user input after a set delay
+- (void)resetInput;
+
+// signal that the passcode is incorrect
 - (void)wrong;
+
+// dismiss the view after a set delay
 - (void)dismiss;
+
 @end
 
 @implementation GCPINViewController
@@ -81,17 +98,16 @@
 	[controller presentModalViewController:navController animated:animated];
 	[navController release];
 }
-- (void)updatePINDisplay {
+- (void)updatePasscodeDisplay {
     NSUInteger length = [self.inputField.text length];
     for (NSUInteger i = 0; i < 4; i++) {
         UILabel *label = [self.labels objectAtIndex:i];
         label.text = (i < length) ? @"●" : @"";
     }
 }
-- (void)reset {
+- (void)resetInput {
     [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
-    double delay = 0.3;
-    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC);
+    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, kGCPINViewControllerDelay * NSEC_PER_SEC);
     dispatch_after(time, dispatch_get_main_queue(), ^(void){
         self.inputField.text = @"";
         [[UIApplication sharedApplication] endIgnoringInteractionEvents];
@@ -100,12 +116,12 @@
 - (void)wrong {
     AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
     self.errorLabel.hidden = NO;
-    [self reset];
+    self.text = nil;
+    [self resetInput];
 }
 - (void)dismiss {
     [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
-    double delay = 0.3;
-    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC);
+    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, kGCPINViewControllerDelay * NSEC_PER_SEC);
     dispatch_after(time, dispatch_get_main_queue(), ^(void){
         [self dismissModalViewControllerAnimated:YES];
         [[UIApplication sharedApplication] endIgnoringInteractionEvents];
@@ -128,7 +144,7 @@
     self.messageLabel.text = self.messageText;
     self.errorLabel.text = self.errorText;
     self.errorLabel.hidden = YES;
-	[self updatePINDisplay];
+	[self updatePasscodeDisplay];
     
 	// setup input field
     self.inputField.hidden = YES;
@@ -166,12 +182,12 @@
 - (void)textDidChange:(NSNotification *)notif {
     if ([notif object] == self.inputField) {
         NSAssert(self.verifyBlock, @"No passcode verify block is set");
-        [self updatePINDisplay];
+        [self updatePasscodeDisplay];
         if ([self.inputField.text length] == 4) {
             if (self.mode == GCPINViewControllerModeCreate) {
                 if (self.text == nil) {
                     self.text = self.inputField.text;
-                    [self reset];
+                    [self resetInput];
                 }
                 else {
                     if ([self.text isEqualToString:self.inputField.text] &&
@@ -180,11 +196,10 @@
                     }
                     else {
                         [self wrong];
-                        self.text = nil;
                     }
                 }
             }
-            else {
+            else if (self.mode == GCPINViewControllerModeVerify) {
                 if (self.verifyBlock(self.inputField.text)) {
                     [self dismiss];
                 }
@@ -192,7 +207,7 @@
                     [self wrong];
                 }
             }
-		}
+        }
     }
 }
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
